6e64f90bfa731e71140ca079ffda48d2d0a90d1c
saitodisse <saitodisse@gmail.com>
2012-03-14 17:59:14 -0300

    console para brincar

---
+++ ConsoleApplicationExtenso/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Extenso;
+
+namespace ConsoleApplicationExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("numero: ");
+                string numero = Console.ReadLine();
+                if(string.IsNullOrEmpty(numero))
+                {
+                    break;
+                }
+                var porExtenso = NumeroExtenso.Converter(numero);
+                Console.WriteLine(porExtenso);
+            }
+        }
+    }
+}
